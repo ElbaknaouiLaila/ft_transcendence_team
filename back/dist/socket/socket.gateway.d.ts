3df43fb6e5976436f12ee01b10aab701213bb172
M'barek Manouze <mmanouze@e1r12p2.1337.ma>
2023-10-25 19:59:18 +0100

    handled the user status in the backend

--- back/dist/socket/socket.gateway.d.ts
+++ back/dist/socket/socket.gateway.d.ts
@@ -1,13 +1,17 @@
 import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { JwtService } from 'src/jwt/jwtservice.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 export declare class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     private jwt;
-    constructor(jwt: JwtService);
+    private readonly prisma;
+    constructor(jwt: JwtService, prisma: PrismaService);
     server: Server;
-    handleConnection(client: Socket): any;
-    handleDisconnect(client: Socket): void;
-    handleUserOnline(client: Socket, payload: any): void;
-    handleUserOffline(client: Socket, payload: any, header: any): void;
+    private SocketContainer;
+    decodeCookie(client: Socket): any;
+    handleConnection(client: Socket): Promise<void>;
+    handleDisconnect(client: Socket): Promise<void>;
+    handleUserOnline(client: Socket): void;
+    handleUserOffline(client: Socket): void;
     handleMessage(body: any): string;
 }
--- back/src/socket/socket.gateway.ts
+++ back/src/socket/socket.gateway.ts
@@ -2,17 +2,18 @@ import { Headers } from '@nestjs/common';
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server ,Socket} from 'socket.io';
 import { JwtService } from 'src/jwt/jwtservice.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @WebSocketGateway()
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect{
   
-  constructor(private jwt :JwtService){}
+  constructor(private jwt :JwtService, private readonly prisma: PrismaService){}
 
   @WebSocketServer() server: Server;
 
-  handleConnection(client: Socket): any {
-    // Handle new client connection
-    // console.log(client);
+  private SocketContainer 
+
+  decodeCookie(client: Socket){
     let cookieHeader : string  = '';
     cookieHeader = client.handshake.headers.cookies.toString(); // Get the entire cookie header as a string
     // // You can now parse and manipulate the cookie data as needed
@@ -22,27 +23,46 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect{
       return acc;
     }, {});
 
-    // console.log(cookies['cookie']);
-    // console.log(this.jwt.verify(cookies['cookie']));
-    // Access a specific cookie
-    // const specificCookie = cookies['cookie'];
-    // const decoded = this.jwt.verify(specificCookie);
-    // console.log('Received cookies:', cookies);
-    // return (decoded);
+    const specificCookie = cookies['cookie'];
+    const decoded = this.jwt.verify(specificCookie);
+
+    return (decoded);
   }
 
-  handleDisconnect(client: Socket) {
-    // Handle client disconnection
+  async handleConnection(client: Socket) {
+
+    console.log('client ' + client.id + ' has conected');
+    const decoded = this.decodeCookie(client);
+    const user = await this.prisma.user.update({
+      where : {id_user : decoded.id},
+      data :{
+        status_user : "online",
+      },
+    });
+    console.log(user);
+  }
+
+  async handleDisconnect(client: Socket) {
+    console.log('client ' + client.id + ' has disconnected');
+    const decoded = this.decodeCookie(client);
+    const user = await this.prisma.user.update({
+      where : {id_user : decoded.id},
+      data :{
+        status_user : "offline",
+      },
+    });
+    console.log(user);
   }
 
   @SubscribeMessage('userOnline')
-  handleUserOnline(client: Socket, payload: any) {
+  handleUserOnline(client: Socket) {
+    this.handleConnection(client);
     // Handle when a user comes online
   }
 
   @SubscribeMessage('userOffline')
-  handleUserOffline(client: Socket, payload: any, @Headers() header) {
-    console.log(client);
+  handleUserOffline(client: Socket) {
+    // this.handleDisconnect(client);
     // this.handleConnection(client);
     // console.log(decoded);
     // console.log('lawaaal');
